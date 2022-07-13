436794d3c824bc2d4974d3756e84a2c194715561
Seungjae Park <astro.psj@gmail.com>
2022-07-13 22:37:39 +0900

    Spring Initializr

---
+++ src/test/java/life/inha/icemarket/IceMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package life.inha.icemarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IceMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
