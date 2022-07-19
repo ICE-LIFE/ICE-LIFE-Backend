ebcd9a0ad5ea7236cad9de01f6bf74e60427a5c3
MingyeomKim <67851124+MingyeomKim@users.noreply.github.com>
2022-07-19 20:34:20 +0900

    [refactor] 디렉토리 추가를 위한 Dummy 파일 생성

--- src/test/java/life/inha/icemarket/domain/auth/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package life.inha.icemarket.domain.auth;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
-    User findById(int id);
-}
