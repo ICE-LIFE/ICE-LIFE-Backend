808b7b6d2221278a764db6674853248063e8438f
MingyeomKim <67851124+MingyeomKim@users.noreply.github.com>
2022-07-21 01:41:54 +0900

    [hotfix] Swagger Dependency 추가 및 ApiReponse 사용

--- src/main/java/life/inha/icemarket/controller/auth/MyPageController.java
+++ src/main/java/life/inha/icemarket/controller/auth/MyPageController.java
@@ -1,5 +1,8 @@
 package life.inha.icemarket.controller.auth;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import life.inha.icemarket.domain.auth.User;
 import life.inha.icemarket.dto.MyPageDto;
 import life.inha.icemarket.service.auth.MyPageService;
@@ -9,21 +12,30 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.nio.charset.Charset;
 
+@Tag(name = "MyPage", description = "마이페이지")
 @RestController
 @RequiredArgsConstructor
 public class MyPageController {
     private MyPageService myPageService;
 
     @GetMapping("/user/mypage")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400", description = "잘못된 유저 조회 URL 요청"),
+            @ApiResponse(responseCode = "403", description = "클라이언트의 접근 권한이 없음")
+    })
     public ResponseEntity<MyPageDto> getUserInfo(User user) { // 추후 Authentication 으로 수정 필요
         HttpHeaders header = new HttpHeaders();
         header.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
         return new ResponseEntity<MyPageDto>(myPageService.getMyPageDto(user), header, HttpStatus.OK);
     }
+
+
+
+
 
 }
