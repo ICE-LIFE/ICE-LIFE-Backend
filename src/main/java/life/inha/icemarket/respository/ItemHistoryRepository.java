08bd00138d51683e296fa2314bffb7579679345b
윤여준 <30434779+Yun-YeoJun@users.noreply.github.com>
2022-07-30 17:31:21 +0900

    Update ItemHistoryRepository.java

--- src/main/java/life/inha/icemarket/respository/ItemHistoryRepository.java
+++ src/main/java/life/inha/icemarket/respository/ItemHistoryRepository.java
@@ -1,10 +1,10 @@
 package life.inha.icemarket.respository;
 
-import life.inha.icemarket.domain.ItemHistrory;
+import life.inha.icemarket.domain.ItemHistory;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ItemHistoryRepository extends CrudRepository<ItemHistrory,Integer> {
+public interface ItemHistoryRepository extends CrudRepository<ItemHistory,Integer> {
     @Query(value = "SELECT COUNT(return_at) FROM item_history WHERE item_id=?1",nativeQuery = true)
     Integer countReturnItem(Integer item_id);
 
