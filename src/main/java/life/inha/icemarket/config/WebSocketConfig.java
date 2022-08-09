66a2b1a97c535e321076113c5397d0d2bea679d7
Seungjae Park <astro.psj@gmail.com>
2022-08-09 18:09:19 +0900

    Apply JWT auth on WS chat

--- src/main/java/life/inha/icemarket/config/WebSocketConfig.java
+++ src/main/java/life/inha/icemarket/config/WebSocketConfig.java
@@ -1,5 +1,11 @@
 package life.inha.icemarket.config;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import life.inha.icemarket.domain.User;
+import life.inha.icemarket.service.UserSecurityService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
@@ -11,19 +17,20 @@ import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private final UserSecurityService userSecurityService;
+
+    public WebSocketConfig(UserSecurityService userSecurityService) {
+        this.userSecurityService = userSecurityService;
+    }
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
@@ -38,12 +45,19 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
             public Message<?> preSend(Message<?> message, MessageChannel channel) {
                 StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
                 if (StompCommand.CONNECT.equals(accessor.getCommand())) {
-                    String username = accessor.getFirstNativeHeader("user");
-                    List<GrantedAuthority> authorities = new ArrayList<>();
-                    authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
-                    Authentication authentication = new UsernamePasswordAuthenticationToken(username, username, authorities);
-                    SecurityContextHolder.getContext().setAuthentication(authentication);
-                    accessor.setUser(authentication);
+                    String accessToken = accessor.getFirstNativeHeader("access_token");
+                    System.out.println("WS : access_token=" + accessToken);
+                    if (accessToken != null) {
+                        Algorithm algorithm = Algorithm.HMAC256("ice-market");
+                        JWTVerifier verifier = JWT.require(algorithm).withIssuer("ice").build();
+                        DecodedJWT jwt = verifier.verify(accessToken);
+                        String email = jwt.getSubject();
+                        System.out.println("Verify JWT on WS : email=" + email);
+                        User user = (User) userSecurityService.loadUserByUsername(email);
+                        Authentication authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                        SecurityContextHolder.getContext().setAuthentication(authentication);
+                        accessor.setUser(authentication);
+                    }
                 }
                 return message;
             }
