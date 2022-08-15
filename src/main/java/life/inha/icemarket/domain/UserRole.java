0dd975b28965bc8f41ab88a908fe836f701247a4
MingyeomKim <67851124+MingyeomKim@users.noreply.github.com>
2022-08-15 11:53:42 +0900

    [refactor] QClass 오류 수정 (#55)

--- src/main/java/life/inha/icemarket/domain/UserRole.java
+++ src/main/java/life/inha/icemarket/domain/UserRole.java
@@ -7,8 +7,7 @@ import lombok.Getter;
 public enum UserRole {
     GUEST("ROLE_GUEST"),
     USER("ROLE_USER"),
-    ADMIN("ROLE_ADMIN"),
-    GUEST("ROLE_GUEST");
+    ADMIN("ROLE_ADMIN");
 
     UserRole(String value){
          this.value = value;
