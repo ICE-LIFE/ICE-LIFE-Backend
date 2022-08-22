24d261149d778a726c3d222bf1839c97a81a8508
Seungjae Park <astro.psj@gmail.com>
2022-08-22 16:06:29 +0900

    Set CORS config

--- src/main/java/life/inha/icemarket/IceMarketApplication.java
+++ src/main/java/life/inha/icemarket/IceMarketApplication.java
@@ -3,11 +3,22 @@ package life.inha.icemarket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class IceMarketApplication {
     public static void main(String[] args) {
         SpringApplication.run(IceMarketApplication.class, args);
+    }
+
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 }
