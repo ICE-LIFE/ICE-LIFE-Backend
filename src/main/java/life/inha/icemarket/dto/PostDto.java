69123e8448f76cc2c21ae68d5de499598cc8805d
Seungjae Park <astro.psj@gmail.com>
2022-08-30 18:59:28 +0900

    Set to return author nickname from post and comment DTO

--- src/main/java/life/inha/icemarket/dto/PostDto.java
+++ src/main/java/life/inha/icemarket/dto/PostDto.java
@@ -8,6 +8,7 @@ import lombok.Data;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -18,6 +19,7 @@ public class PostDto {
     private Integer id;
     private Integer categoryId;
     private Integer authorId;
+    private String authorNickname;
     private String title;
     private String content;
     private String thumbnail;
@@ -29,6 +31,7 @@ public class PostDto {
                 .id(post.getId())
                 .categoryId(post.getCategory().getId())
                 .authorId(post.getUser().getId())
+                .authorNickname(Objects.requireNonNullElse(post.getUser().getNickname(), post.getUser().getName()))
                 .title(post.getTitle())
                 .content(post.getContent())
                 .thumbnail(post.getThumbnail())
