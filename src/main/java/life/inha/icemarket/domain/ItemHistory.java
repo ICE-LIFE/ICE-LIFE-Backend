5acc33d9a01410e90e725dfec3059b32c4a3397e
yunyeojun <yyj8771@naver.com>
2022-08-31 19:56:59 +0900

    [Feature/#45] 복지물품 게시판 테스트 코드 작성

--- src/main/java/life/inha/icemarket/domain/ItemHistory.java
+++ src/main/java/life/inha/icemarket/domain/ItemHistory.java
@@ -1,9 +1,6 @@
 package life.inha.icemarket.domain;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,6 +10,7 @@ import java.sql.Timestamp;
 
 @NoArgsConstructor
 @Getter
+@Setter
 @Entity(name = "item_history")
 public class ItemHistory {
     @Id
